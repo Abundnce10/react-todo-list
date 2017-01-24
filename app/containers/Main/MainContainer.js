926fa71fe54c5fde22e65b9b408f4b11d314791d
Abundnce10 <michaelstitt@gmail.com>
2017-01-23 22:20:43 -0800

    Add a form input that updates the state

--- app/containers/Main/MainContainer.js
+++ app/containers/Main/MainContainer.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { FormControl } from 'react-bootstrap'
 import { ToDosContainer } from 'containers'
 
 class MainContainer extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      temporaryInput: "",
       items: [
         {
           finished: false,
@@ -16,16 +18,36 @@ class MainContainer extends React.Component {
         }
       ]
     }
+
+    this.handleFormOnChange = this.handleFormOnChange.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+
+  handleFormOnChange(event) {
+    this.setState({temporaryInput: event.target.value});
+  }
+
+  handleFormSubmit(event) {
+    let items = this.state.items
+    // add temporary to items array
+    items.unshift({finished: false, description: this.state.temporaryInput})
+    // setState (reset tempoorary to empty string )
+    this.setState({items: items, temporaryInput: ""})
+    event.preventDefault();
   }
 
   render() {
     return (
       <div className="container">
         <div className="row">  
-          <div className="col-sm-8 col-sm-offset-2">
-            <div className="text-center">
-              <h1>To Do List</h1>
-              <h3>Form</h3>
+          <div className="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+            <div>
+              <h1 className="text-center">todos</h1>
+
+              <form onSubmit={this.handleFormSubmit} className="text-center">
+                <FormControl type="text" value={this.state.temporaryInput} onChange={this.handleFormOnChange} />
+              </form>
+
               <ToDosContainer items={this.state.items} />
               <h4>Filters</h4>
             </div>
