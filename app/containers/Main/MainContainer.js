b56299c45c3967657ae5a5b9daf290a03d889e8e
Abundnce10 <michaelstitt@gmail.com>
2017-01-23 21:10:51 -0800

    Mock up the various pieces needed

--- app/containers/Main/MainContainer.js
+++ app/containers/Main/MainContainer.js
@@ -1,10 +1,20 @@
 import React from 'react'
-import { color } from './styles.css'
 
 const MainContainer = React.createClass({
   render () {
     return (
-      <p className={color}>{'React To Do List'}</p>
+      <div className="container">
+        <div className="row">  
+          <div className="col-sm-8 col-sm-offset-2">
+            <div className="text-center">
+              <h1>To Do List</h1>
+              <h3>Form</h3>
+              <h3>To Do List</h3>
+              <h4>Filters</h4>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   },
 })
